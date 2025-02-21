75e851f4719c2a9d14c8d04e2d0a4c393107ca67
Letlotlo Mokuoa <Lytlotlo@users.noreply.huggingface.co>
2025-02-21 09:58:12 +0000

    add tool

--- app.py
+++ app.py
@@ -100,7 +100,8 @@ with open("prompts.yaml", 'r') as stream:
 agent = CodeAgent(
     model=model,
     tools=[final_answer,
-          get_current_time_in_timezone], ## add your tools here (don't remove final answer)
+          get_current_time_in_timezone,
+          get_ai_safety_updates], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
